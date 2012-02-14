8fb2105ae6506215f6fcfba37c23d70a6ff8db64
David Hart <dhart1@unca.edu>
2012-02-14 16:54:25 -0500

    added search box and got twitter button working, still working on facebook button

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -3,52 +3,69 @@
  */
 
 function main() {
-    var s = new Spotter("twitter.search",
-					{q:"asheville", period:120},
+	var searchText = $("#input_text");
+	$("#twitter_button").click(function(){
+	 	var s = new Spotter("twitter.search",
+					{q:searchText, period:120},
 					{buffer:true, bufferTimeout:750}
 					);
-	var color = 'red';
-	var count=0;
-	var object_array = [];
-
-   s.register(function(tweet){
-		count++;
-		var profile_image="<img src='"+tweet.profile_image_url+"'/>";
-		if(count%2===0) {
-			color='red';
-		}
-		else {
-			color='blue';
-		}
-		var object=$("<p class="+color+">"+profile_image + tweet.text +"</p>");
-		object.hide();
-		$("#tweets").prepend(object);
-		object.slideDown();
-		
-
-		$("#tweets p:gt(5)").fadeOut(200, function (){
-			$("#tweets p:gt(5)").remove();
-		});
-		
-		/*
-		if(count > 4){	
-		var object_to_remove = $("#tweets p: last-child");
-		object_to_remove.fadeout(1000, function () {
-			object_to_remove.remove();
-		});
-		}*/
-		
-		/*object_array.push(object);
-		if(object_array.length>5){
-			var object_to_remove=object_array.shift();
-			object_to_remove.fadeOut(2000, function() {
-				object_to_remove.remove();	
-			});
-		}*/
+					
+		var color = 'red';
+		var count=0;
+		var object_array = [];
+		s.register(function(tweet){
+			count++;
+			var profile_image="<img src='"+tweet.profile_image_url+"'/>";
+			if(count%2===0) {
+				color='red';
+			}
+			else {
+				color='blue';
+			}
+			var object=$("<p class="+color+">"+profile_image + tweet.text +"</p>");
+			object.hide();
+			$("#tweets").prepend(object);
+			object.slideDown();
 			
-	});
-	s.start();
+			$("#tweets p:gt(5)").fadeOut(200, function (){
+				$("#tweets p:gt(5)").remove();
+			});
+			
+		});	
+		s.start();
+		$("#facebook_button").click(function(){
+		 	var f = new Spotter("facebook.search",
+						{q:searchText, period:120},
+						{buffer:true, bufferTimeout:750}
+						);
+						
+			var color = 'red';
+			var count=0;
+			var object_array = [];
+			f.register(function(statuses){
+				count++;
+				alert("hello!")
+				var profile_image="<img src='"+facebook.profile_image_url+"'/>";
+				if(count%2===0) {
+					color='red';
+				}
+				else {
+					color='blue';
+				}
+				var object=$("<p class="+color+">"+profile_image_url+statuses.text+"</p>");
+				object.hide();
+				$("#posts").prepend(object);
+				object.slideDown();
 
+				$("#posts p:gt(5)").fadeOut(200, function (){
+					$("#posts p:gt(5)").remove();
+				});
+			});	
+			f.start();
+	});
 }
 
-main();
+
+$(document).ready(function() {
+	main();
+});
