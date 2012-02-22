631eb1439cad53ffb212ea0f5fc50a8d35cc079d
David Hart <dhart1@unca.edu>
2012-02-22 10:59:47 -0500

    added scoreboard and keyword searchboxes. Facebook and Twitter feeds are working. scoreboard is working.

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -3,68 +3,108 @@
  */
 
 function main() {
-	var searchText = $("#input_text");
 	$("#twitter_button").click(function(){
+		//$("#input_text").keyup(fucntion(e)){
+		//	if(e.keyCode== "13"){
+		var searchText = $("#input_text").val();
+		var word1 = new RegExp("/"+word1_input+"/");
+		var word1_count = 0;
+		var word2 = new RegExp("/"+word2_input+"/");
+		var word2_count = 0;
+		var word1_input = $("#word1_text").val();
+		var word2_input = $("#word2_text").val();
+		$("#result_1").empty();
+		$("#result_2").empty();
+		$("#result_1").append(word1_input);
+		$("#result_2").append(word2_input);
+
 	 	var s = new Spotter("twitter.search",
-					{q:searchText, period:120},
+					{q:searchText, period:60},
 					{buffer:true, bufferTimeout:750}
 					);
-					
-		var color = 'red';
-		var count=0;
-		var object_array = [];
+		var object_array = [];		
 		s.register(function(tweet){
-			count++;
 			var profile_image="<img src='"+tweet.profile_image_url+"'/>";
-			if(count%2===0) {
+			/*if(count%2===0) {
 				color='red';
 			}
 			else {
 				color='blue';
-			}
-			var object=$("<p class="+color+">"+profile_image + tweet.text +"</p>");
+			}*/
+			var object=$("<p class='red'>"+profile_image + tweet.text +"</p>");
 			object.hide();
 			$("#tweets").prepend(object);
 			object.slideDown();
 			
-			$("#tweets p:gt(5)").fadeOut(200, function (){
-				$("#tweets p:gt(5)").remove();
-			});
+			if(tweet.text.match(word1_input)) {
+				word1_count++;
+			}
+			$("#twitter_hits1").empty();
+			$("#twitter_hits1").append(word1_count);
 			
+			if(tweet.text.match(word2_input)) {
+				word2_count++;
+			}
+			$("#twitter_hits2").empty();
+			$("#twitter_hits2").append(word2_count);
+			
+			$("#tweets p:gt(5)").fadeOut(200, function (){
+				$("#posts p:gt(5)").remove();
+			});
 		});	
 		s.start();
+	//		}
+	//	});
+	});
+
+
 		$("#facebook_button").click(function(){
+			var searchText = $("#input_text").val();
+			var word1 = new RegExp("/"+word1_input+"/");
+			var word1_count=0;
+			var word2 = new RegExp("/"+word2_input+"/");
+			var word2_count=0;
+			
+			var word1_input = $("#word1_text").val();
+			var word2_input = $("#word2_text").val();
+			$("#result_1").empty();
+			$("#result_2").empty();
+			$("#result_1").append(word1_input);
+			$("#result_2").append(word2_input);
+			
+			
 		 	var f = new Spotter("facebook.search",
-						{q:searchText, period:120},
+						{q:searchText, period:60},
 						{buffer:true, bufferTimeout:750}
 						);
-						
-			var color = 'red';
-			var count=0;
 			var object_array = [];
-			f.register(function(statuses){
-				count++;
-				alert("hello!")
-				var profile_image="<img src='"+facebook.profile_image_url+"'/>";
-				if(count%2===0) {
-					color='red';
-				}
-				else {
-					color='blue';
-				}
-				var object=$("<p class="+color+">"+profile_image_url+statuses.text+"</p>");
+			f.register(function(status){
+				var profile_image="<img src='"+status.profile_image_url+"'/>";
+				var object=$("<p class='blue'>"+status.profile_url+profile_image+status.message+"</p>");
 				object.hide();
 				$("#posts").prepend(object);
 				object.slideDown();
+				
+				if(status.message.match(word1_input)) {
+					word1_count++;
+				}
+				$("#facebook_hits1").empty();
+				$("#facebook_hits1").append(word1_count);
 
+				if(status.message.match(word2_input)) {
+					word2_count++;
+				}
+				$("#facebook_hits2").empty();
+				$("#facebook_hits2").append(word2_count);
+				
 				$("#posts p:gt(5)").fadeOut(200, function (){
 					$("#posts p:gt(5)").remove();
 				});
-			});	
-			f.start();
-	});
-}
+			});
+			f.start();	
+		});
 
+}
 
 $(document).ready(function() {
 	main();
